d4416f6c2a81e0b48d6d534db892bea482da04ca
Francis Lau <francis.k.lau@gmail.com>
2020-09-16 13:09:35 -0700

    End of 193

--- models/product.js
+++ models/product.js
@@ -2,17 +2,33 @@ const mongodb = require('mongodb');
 const getDb = require('../util/database').getDb;
 
 class Product {
-  constructor(title, price, description, imageUrl) {
+  constructor(title, price, description, imageUrl, id) {
     this.title = title;
     this.price = price;
     this.description = description;
     this.imageUrl = imageUrl;
+    // if (id) {
+    //   this._id = new mongodb.ObjectID(id);
+    // } else {
+    //   this._id = null;
+    // }
+    this._id = id ? new mongodb.ObjectID(id) : null;
   }
 
   save() {
     const db = getDb();
-    return db.collection('products')
-      .insertOne(this)
+    let dbOp;
+    if (this._id) {
+      // update the product
+      dbOp = db
+        .collection('products')
+        .updateOne({_id: this._id}, {$set: this});
+    } else {
+      // creare new product
+      dbOp = db.collection('products')
+      .insertOne(this) ;
+    }
+    return dbOp
       .then(result => {
         console.log(result);
       })
@@ -48,6 +64,18 @@ class Product {
     .catch(err => {
       console.log(err);
     });
+  }
+
+  static deleteById(prodId) {
+    const db = getDb();
+    return db.collection('products')
+      .deleteOne({ _id: new mongodb.ObjectID(prodId)})
+      .then(() => {
+        console.log('Deleted!');
+      })
+      .catch(err => {
+        console.log(err);
+      });
   }
 }
 

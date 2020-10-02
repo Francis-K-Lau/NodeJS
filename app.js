f6e76e86a12c6bc690c584335bfa938c8de5b07c
Francis Lau <francis.k.lau@gmail.com>
2020-10-02 11:43:15 -0700

    Session14.240

--- app.js
+++ app.js
@@ -3,11 +3,20 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const session = require('express-session');
+const mongoDBStore = require('connect-mongodb-session')(session);
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
 
+const MONGODB_URL = 'mongodb+srv://FrancisLau:S37mwfjAOixwklQl@cluster0.qy515.mongodb.net/shop';
+
+
 const app = express();
+const store = new mongoDBStore({
+  uri: MONGODB_URL, 
+  collection: 'sessions'
+});
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -18,6 +27,13 @@ const authRoutes = require('./routes/auth');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(
+  session({ secret: 'my secret', 
+  resave: false, 
+  saveUninitialized: false,
+  store: store
+  })
+);
 
 app.use((req, res, next) => {
   User.findById('5f72639b4a0024333c511d57')
@@ -35,7 +51,7 @@ app.use(authRoutes);
 app.use(errorController.get404);
 
 mongoose
-.connect('mongodb+srv://FrancisLau:S37mwfjAOixwklQl@cluster0.qy515.mongodb.net/shop?retryWrites=true&w=majority')
+.connect(MONGODB_URL)
   .then(result => {
     User.findOne().then(user => {
       if (!user) {
--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,15 @@
 exports.getLogin = (req, res, next) => {
+//  console.log(req.get('Cookie').split(';')[1]);
+  console.log(req.session.isLoggedIn);
   res.render('auth/login', {
     path: '/login',
-    pageTitle: 'Login'
+    pageTitle: 'Login',
+    isAuthenticated: false
   });
+};
+
+exports.postLogin = (req, res, next) => {
+//  res.setHeader('Set-Cookie', 'loggedIn=true');
+  res.session.isLoggedIn = true;
+  res.render('/');
 };
